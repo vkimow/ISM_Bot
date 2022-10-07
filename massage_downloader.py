4176d8ce0d4ea3efe2da2222a3590a77bf28caee
Egor Akimov <egorvkimow@gmail.com>
2022-10-07 03:28:42 +0300

    add folders if didnt exists

--- massage_downloader.py
+++ massage_downloader.py
@@ -3,6 +3,16 @@ from google_parser import Parser
 from config import Paths
 
 def load(services, files_to_download):
+    def create_folder(folder_path):
+        if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+
+    create_folder(Paths.resource)
+    create_folder(Paths.actions)
+    create_folder(Paths.anatomy)
+    create_folder(Paths.backgrounds)
+    create_folder(Paths.specialists)
+
     for root, dir, files in os.walk(Paths.resource):
         for file in files:
             os.remove(os.path.join(root, file))
