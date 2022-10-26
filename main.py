81d49516c0876916385db3ccfc8ead919b95b4e7
Egor Akimov <egorvkimow@gmail.com>
2022-10-27 00:04:41 +0300

    change bot token

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from google.google_service_creator import create_google_drive_service, create_go
 from google.google_service_container import GoogleServices
 
 
-bot = Bot(  telegram_bot = telebot.TeleBot(config.BotTokens.main),
+bot = Bot(  telegram_bot = telebot.TeleBot(config.BotTokens.test),
             google_services = GoogleServices(create_google_drive_service(), create_google_sheet_service()),
             data = None)
 
